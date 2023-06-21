266553639b150c324fbb6706f669b06fa2c4a583
Mr.Lei <1832244342@qq.com>
2023-06-21 12:41:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,14 +14,14 @@ const USER_CONFIG = {
   APP_SECRET: '1a1dfcc00cd2ef7322f058d39b72d31b',
 
   PROVINCE: '云南',
-  CITY: '腾冲',
+  CITY: '保山',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ob2pD6TR4sGDryjfYeC0FO5Z-gyg	',
+      id: 'ob2pD6TR4sGDryjfYeC0FO5Z-gyg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'zZwt7sZdwnQruMLg41w3cdgcMMR1Xsi76_vPnglXy00',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'jjVmaIfUColRjaaTaq2PCQTrfBNWnLnfhzWQ9Y3ZZ9A',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ob2pD6TR4sGDryjfYeC0FO5Z-gyg',
     }
   ],
 
